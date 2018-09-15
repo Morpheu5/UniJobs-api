9b96de3aee2e2ed0c59d03f8a56e745ce7b7d97c
Andrea Franceschini <andrea.franceschini@gmail.com>
2018-09-15 09:42:09 +0100

    :bug: Delete content_blocks when content is deleted.

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Content < ApplicationRecord
-  has_many :content_blocks, -> { order(order: :asc) }, inverse_of: :content
+  has_many :content_blocks, -> { order(order: :asc) }, inverse_of: :content, dependent: :destroy
   belongs_to :organization, inverse_of: :organizations
   belongs_to :user
   after_create :reload_uuid
