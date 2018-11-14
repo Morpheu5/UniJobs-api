424849619aa4c40f58ec9a04987f6b153dabec8c
Andrea Franceschini <andrea.franceschini@gmail.com>
2018-11-14 10:10:44 +0000

    :bookmark: Release 0.2.1

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,6 @@ module Api
 
     config.site_baseurl = Rails.env.production? ? ENV['SITE_BASEURL'] : 'http://localhost:8080'
 
-    config.api_version = '0.2.0'
+    config.api_version = '0.2.1'
   end
 end
