b0c9ce506288351ff756e48e4a44f42bdac45888
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-10-18 16:55:20 +0100

    :white_check_mark: Unbreak tests

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -10,30 +10,30 @@ RSpec.context 'When logged out' do
 
   describe 'access to all users' do
     it 'fails' do
-      get '/api/users'
+      get '/users'
       expect(response).to have_http_status(:unauthorized)
     end
   end
 
   describe 'access to one user' do
     it 'is successful' do
-      get "/api/users/#{[*@some_users, *@some_admins].sample.id}"
+      get "/users/#{[*@some_users, *@some_admins].sample.id}"
       expect(response).to have_http_status(:ok)
     end
 
     it 'retrieves the correct user' do
       user = [*@some_users, *@some_admins].sample
-      get "/api/users/#{user.id}"
+      get "/users/#{user.id}"
       expect(json['id']).to equal(user.id)
     end
 
     it 'does not return the password digest' do
-      get "/api/users/#{[*@some_users, *@some_admins].sample.id}"
+      get "/users/#{[*@some_users, *@some_admins].sample.id}"
       expect(json['password_digest']).to equal(nil)
     end
 
     it 'does not return the verification token' do
-      get "/api/users/#{[*@some_users, *@some_admins].sample.id}"
+      get "/users/#{[*@some_users, *@some_admins].sample.id}"
       expect(json['verification_token']).to equal(nil)
     end
   end
@@ -44,24 +44,24 @@ RSpec.context 'When logged out' do
     end
 
     it 'is successful' do
-      post '/api/users', params: { user: @new_user }
+      post '/users', params: { user: @new_user }
       expect(response).to have_http_status(:created)
     end
 
     it 'only creates regular users' do
-      post '/api/users', params: { user: FactoryBot.attributes_for(:admin) }
+      post '/users', params: { user: FactoryBot.attributes_for(:admin) }
       expect(json['role']).to eq('USER')
     end
 
     ### TODO: Maybe have checks for all the parameters
 
     it 'does not return the password digest' do
-      post '/api/users', params: { user: @new_user }
+      post '/users', params: { user: @new_user }
       expect(json['password_digest']).to equal(nil)
     end
 
     it 'does not return the verification token' do
-      post '/api/users', params: { user: @new_user }
+      post '/users', params: { user: @new_user }
       expect(json['verification_token']).to equal(nil)
     end
   end
@@ -72,19 +72,19 @@ RSpec.context 'When logged out' do
     end
 
     it 'fails with PATCH' do
-      patch "/api/users/#{@new_user.id}", params: { user: { given_name: 'Testing Alice' } }
+      patch "/users/#{@new_user.id}", params: { user: { given_name: 'Testing Alice' } }
       expect(response).to have_http_status(:forbidden)
       expect(User.find(@new_user.id).given_name).to_not eq('Testing Alice')
     end
 
     it 'fails with PUT' do
-      put "/api/users/#{@new_user.id}", params: { user: { family_name: 'Testing Smith' } }
+      put "/users/#{@new_user.id}", params: { user: { family_name: 'Testing Smith' } }
       expect(response).to have_http_status(:forbidden)
       expect(User.find(@new_user.id).family_name).to_not eq('Testing Smith')
     end
 
     it 'fails to update the password even if old_password is provided correctly' do
-      put "/api/users/#{@new_user.id}", params: { user: { old_password: 'testpassword', password: 'newpassword!' } }
+      put "/users/#{@new_user.id}", params: { user: { old_password: 'testpassword', password: 'newpassword!' } }
       expect(response).to have_http_status(:forbidden)
     end
   end
@@ -99,17 +99,17 @@ RSpec.context 'When logged out' do
     end
 
     it 'is successful with the right credentials' do
-      post '/api/login', params: { email: @new_user.email, password: 'testpassword' }
+      post '/login', params: { email: @new_user.email, password: 'testpassword' }
       expect(response).to have_http_status(:ok)
     end
 
     it 'fails with the wrong password' do
-      post '/api/login', params: { email: @new_user.email, password: 'wrongpassword' }
+      post '/login', params: { email: @new_user.email, password: 'wrongpassword' }
       expect(response).to have_http_status(:forbidden)
     end
 
     it 'fails with the wrong credentials' do
-      post '/api/login', params: { email: 'somerandomemail@example.com', password: 'wrongpassword' }
+      post '/login', params: { email: 'somerandomemail@example.com', password: 'wrongpassword' }
       expect(response).to have_http_status(:forbidden)
     end
   end
@@ -124,36 +124,36 @@ RSpec.context 'When logged in' do
 
   context 'as a user' do
     before(:each) do
-      post '/api/login', params: { email: @user.email, password: 'testpassword'}
+      post '/login', params: { email: @user.email, password: 'testpassword'}
       @token = json['token']
       @auth_headers = { Authorization: "Bearer #{@token}" }
     end
 
     describe 'access to all users' do
       it 'fails' do
-        get '/api/users', headers: { **@auth_headers }
+        get '/users', headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
       end
     end
     
     describe 'access to one user' do
       it 'is successful' do
-        get "/api/users/#{@other_user.id}", headers: { **@auth_headers }
+        get "/users/#{@other_user.id}", headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
       end
   
       it 'retrieves the correct user' do
-        get "/api/users/#{@other_user.id}", headers: { **@auth_headers }
+        get "/users/#{@other_user.id}", headers: { **@auth_headers }
         expect(json['id']).to equal(@other_user.id)
       end
 
       it 'does not return the password digest' do
-        get "/api/users/#{@other_user.id}", headers: { **@auth_headers }
+        get "/users/#{@other_user.id}", headers: { **@auth_headers }
         expect(json['password_digest']).to equal(nil)
       end
   
       it 'does not return the verification token' do
-        get "/api/users/#{@other_user.id}", headers: { **@auth_headers }
+        get "/users/#{@other_user.id}", headers: { **@auth_headers }
         expect(json['verification_token']).to equal(nil)
       end  
     end
@@ -164,77 +164,77 @@ RSpec.context 'When logged in' do
       end
 
       it 'fails' do
-        post '/api/users', params: { user: @new_user }, headers: { **@auth_headers }
+        post '/users', params: { user: @new_user }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
       end
     end
 
     describe 'user update' do
       it 'succeeds with PATCH' do
-        patch "/api/users/#{@user.id}", params: { user: { given_name: 'Testing Alice' } }, headers: { **@auth_headers }
+        patch "/users/#{@user.id}", params: { user: { given_name: 'Testing Alice' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
         expect(json['given_name']).to eq('Testing Alice')
       end
   
       it 'succeeds with PUT' do
-        put "/api/users/#{@user.id}", params: { user: { family_name: 'Testing Smith' } }, headers: { **@auth_headers }
+        put "/users/#{@user.id}", params: { user: { family_name: 'Testing Smith' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
         expect(json['family_name']).to eq('Testing Smith')
       end
   
       it 'updates the password when the old_password is correct' do
-        put "/api/users/#{@user.id}", params: { user: { old_password: 'testpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
+        put "/users/#{@user.id}", params: { user: { old_password: 'testpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
       end
 
       it 'does not update the password when the old_password is wrong' do
-        put "/api/users/#{@user.id}", params: { user: { old_password: 'wrongtestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
+        put "/users/#{@user.id}", params: { user: { old_password: 'wrongtestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
       end
 
       it 'fails to update own role' do
-        put "/api/users/#{@user.id}", params: { user: { role: 'ADMIN' } }, headers: { **@auth_headers }
+        put "/users/#{@user.id}", params: { user: { role: 'ADMIN' } }, headers: { **@auth_headers }
         expect(User.find(@user.id).role).to eq('USER')
       end
     end
 
     describe 'other user update' do
       it 'fails with PATCH' do
-        patch "/api/users/#{@other_user.id}", params: { user: { given_name: 'Testing Alice' } }, headers: { **@auth_headers }
+        patch "/users/#{@other_user.id}", params: { user: { given_name: 'Testing Alice' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
         expect(User.find(@other_user.id).given_name).to_not eq('Testing Alice')
       end
   
       it 'fails with PUT' do
-        put "/api/users/#{@other_user.id}", params: { user: { family_name: 'Testing Smith' } }, headers: { **@auth_headers }
+        put "/users/#{@other_user.id}", params: { user: { family_name: 'Testing Smith' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
         expect(User.find(@other_user.id).family_name).to_not eq('Testing Smith')
       end
   
       it 'does not update the password when the old_password is correct' do
-        put "/api/users/#{@other_user.id}", params: { user: { old_password: 'othertestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
+        put "/users/#{@other_user.id}", params: { user: { old_password: 'othertestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
       end
 
       it 'does not update the password when the old_password is wrong' do
-        put "/api/users/#{@other_user.id}", params: { user: { old_password: 'wrongtestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
+        put "/users/#{@other_user.id}", params: { user: { old_password: 'wrongtestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
       end
 
       it 'fails to update role' do
-        put "/api/users/#{@other_user.id}", params: { user: { role: 'ADMIN' } }, headers: { **@auth_headers }
+        put "/users/#{@other_user.id}", params: { user: { role: 'ADMIN' } }, headers: { **@auth_headers }
         expect(User.find(@other_user.id).role).to eq('USER')
       end
     end
 
     describe 'logout log out' do
       it 'succeeds' do
-        post '/api/logout', headers: { **@auth_headers }
+        post '/logout', headers: { **@auth_headers }
         expect(response).to have_http_status(:no_content)
       end
 
       it 'removes the token' do
-        post '/api/logout', headers: { **@auth_headers }
+        post '/logout', headers: { **@auth_headers }
         expect(AuthenticationToken.where(user: @user, token: @token)).to_not exist()
       end
     end
@@ -242,14 +242,14 @@ RSpec.context 'When logged in' do
 
   context 'as an admin' do
     before(:each) do
-      post '/api/login', params: { email: @admin.email, password: 'admintestpassword'}
+      post '/login', params: { email: @admin.email, password: 'admintestpassword'}
       @token = json['token']
       @auth_headers = { Authorization: "Bearer #{@token}" }
     end
 
     describe 'access to all users' do
       it 'succeeds' do
-        get '/api/users', headers: { **@auth_headers }
+        get '/users', headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
       end
 
@@ -258,22 +258,22 @@ RSpec.context 'When logged in' do
     
     describe 'access to one user' do
       it 'is successful' do
-        get "/api/users/#{@other_user.id}", headers: { **@auth_headers }
+        get "/users/#{@other_user.id}", headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
       end
   
       it 'retrieves the correct user' do
-        get "/api/users/#{@other_user.id}", headers: { **@auth_headers }
+        get "/users/#{@other_user.id}", headers: { **@auth_headers }
         expect(json['id']).to equal(@other_user.id)
       end
 
       it 'does not return the password digest' do
-        get "/api/users/#{@other_user.id}", headers: { **@auth_headers }
+        get "/users/#{@other_user.id}", headers: { **@auth_headers }
         expect(json['password_digest']).to equal(nil)
       end
   
       it 'does not return the verification token' do
-        get "/api/users/#{@other_user.id}", headers: { **@auth_headers }
+        get "/users/#{@other_user.id}", headers: { **@auth_headers }
         expect(json['verification_token']).to equal(nil)
       end  
     end
@@ -284,65 +284,65 @@ RSpec.context 'When logged in' do
       end
 
       it 'fails' do
-        post '/api/users', params: { user: @new_user }, headers: { **@auth_headers }
+        post '/users', params: { user: @new_user }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
       end
     end
 
     describe 'user update' do
       it 'succeeds with PATCH' do
-        patch "/api/users/#{@admin.id}", params: { user: { given_name: 'Testing Alice' } }, headers: { **@auth_headers }
+        patch "/users/#{@admin.id}", params: { user: { given_name: 'Testing Alice' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
         expect(json['given_name']).to eq('Testing Alice')
       end
   
       it 'succeeds with PUT' do
-        put "/api/users/#{@admin.id}", params: { user: { family_name: 'Testing Smith' } }, headers: { **@auth_headers }
+        put "/users/#{@admin.id}", params: { user: { family_name: 'Testing Smith' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
         expect(json['family_name']).to eq('Testing Smith')
       end
   
       it 'updates the password when the old_password is correct' do
-        put "/api/users/#{@admin.id}", params: { user: { old_password: 'admintestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
+        put "/users/#{@admin.id}", params: { user: { old_password: 'admintestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
       end
 
       it 'does not update the password when the old_password is wrong' do
-        put "/api/users/#{@admin.id}", params: { user: { old_password: 'wrongtestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
+        put "/users/#{@admin.id}", params: { user: { old_password: 'wrongtestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
       end
 
       it 'can update own role' do
-        put "/api/users/#{@admin.id}", params: { user: { role: 'USER' } }, headers: { **@auth_headers }
+        put "/users/#{@admin.id}", params: { user: { role: 'USER' } }, headers: { **@auth_headers }
         expect(User.find(@admin.id).role).to eq('USER')
       end
     end
 
     describe 'other user update' do
       it 'succeeds with PATCH' do
-        patch "/api/users/#{@other_user.id}", params: { user: { given_name: 'Testing Alice' } }, headers: { **@auth_headers }
+        patch "/users/#{@other_user.id}", params: { user: { given_name: 'Testing Alice' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
         expect(User.find(@other_user.id).given_name).to eq('Testing Alice')
       end
   
       it 'succeeds with PUT' do
-        put "/api/users/#{@other_user.id}", params: { user: { family_name: 'Testing Smith' } }, headers: { **@auth_headers }
+        put "/users/#{@other_user.id}", params: { user: { family_name: 'Testing Smith' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:ok)
         expect(User.find(@other_user.id).family_name).to eq('Testing Smith')
       end
   
       it 'does not update the password even if the old_password is correct' do
-        put "/api/users/#{@other_user.id}", params: { user: { old_password: 'othertestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
+        put "/users/#{@other_user.id}", params: { user: { old_password: 'othertestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
       end
 
       it 'does not update the password when the old_password is wrong' do
-        put "/api/users/#{@other_user.id}", params: { user: { old_password: 'wrongtestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
+        put "/users/#{@other_user.id}", params: { user: { old_password: 'wrongtestpassword', password: 'newpassword!' } }, headers: { **@auth_headers }
         expect(response).to have_http_status(:forbidden)
       end
 
       it 'can update role' do
-        put "/api/users/#{@other_user.id}", params: { user: { role: 'ADMIN' } }, headers: { **@auth_headers }
+        put "/users/#{@other_user.id}", params: { user: { role: 'ADMIN' } }, headers: { **@auth_headers }
         expect(User.find(@other_user.id).role).to eq('ADMIN')
       end
     end
