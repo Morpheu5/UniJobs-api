2d11e9281e28b7c7ed358604441b12096c981033
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-11-15 12:10:43 +0000

    :bug: Fix up CORS for good, hopefully.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins   Rails.env.development? ? ['localhost'] : ['www.unijobs.it', 'editor.unijobs.it']
+    origins   Rails.env.development? ? [%r{https?://localhost.*}] : [%r{https://.*\.unijobs\.it}]
 
     resource  '*',
               headers: :any,
